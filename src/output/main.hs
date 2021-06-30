58978e7b5e5400e19f7326425f4c3fbfcfa1bc77
karolux4 <karolisbutkus4@gmail.com>
2021-06-30 10:49:34 +0200

    Finished detailed language specification

--- src/output/main.hs
+++ src/output/main.hs
@@ -2,67 +2,36 @@ import Sprockell
 
 prog :: [Instruction]
 prog = [ 
-   Jump (Abs (1))
+   Jump (Abs (16))
+ , Load (ImmValue (0)) regB
+ , Compute Sub regA regB regSP
+ , Load (ImmValue (0)) regB
+ , Compute Sub regA regB regB
+ , Load (IndAddr regB) regB
+ , Load (ImmValue (1)) regC
+ , Compute Add regB regC regB
+ , WriteInstr regB numberIO
+ , Compute Incr regA reg0 regB
+ , Compute Incr regB reg0 regC
+ , Compute Add regC reg0 regSP
+ , Compute Incr regSP reg0 regSP
+ , Load (IndAddr regB) regA
+ , Load (IndAddr regC) regC
+ , Jump (Ind regC)
  , Push regSP
  , Pop regA
  , Compute Decr regA reg0 regA
- , Branch regSprID (Rel (2))
- , Jump (Rel (6))
- , ReadInstr (IndAddr regSprID)
- , Receive regB
- , Compute Equal regB reg0 regC
- , Branch regC (Rel (-3))
- , Jump (Ind regB)
  , Load (ImmValue (0)) regB
  , Compute Sub regSP regB regSP
- , Load (ImmValue (3)) regB
- , WriteInstr regB (DirAddr (3))
- , Load (ImmValue (18)) regB
- , WriteInstr regB (DirAddr (1))
- , Jump (Abs (51))
- , Load (ImmValue (0)) regB
- , Compute Sub regSP regB regSP
- , Load (ImmValue (23)) regB
- , WriteInstr regB (DirAddr (2))
- , Jump (Abs (38))
- , Load (ImmValue (0)) regB
- , Compute Sub regSP regB regSP
- , TestAndSet (DirAddr (0))
- , Receive regB
- , Branch regB (Rel (2))
- , Jump (Rel (-3))
- , ReadInstr (DirAddr (3))
- , Receive regB
- , Load (ImmValue (1)) regC
- , Compute Add regB regC regB
- , Load (ImmValue (3)) regC
- , WriteInstr regB (IndAddr regC)
- , WriteInstr reg0 (DirAddr (0))
- , WriteInstr reg0 (IndAddr regSprID)
- , EndProg
- , TestAndSet (DirAddr (0))
- , Receive regB
- , Branch regB (Rel (2))
- , Jump (Rel (-3))
- , ReadInstr (DirAddr (3))
- , Receive regB
- , Load (ImmValue (2)) regC
- , Compute Add regB regC regB
- , Load (ImmValue (3)) regC
- , WriteInstr regB (IndAddr regC)
- , WriteInstr reg0 (DirAddr (0))
- , WriteInstr reg0 (IndAddr regSprID)
- , EndProg
- , ReadInstr (DirAddr (1))
- , Receive regB
- , ReadInstr (DirAddr (2))
- , Receive regC
- , Compute Or regB regC regB
- , Branch regB (Rel (-5))
- , ReadInstr (DirAddr (3))
- , Receive regB
- , WriteInstr regB numberIO
+ , Load (ImmValue (29)) regB
+ , Push regB
+ , Push regA
+ , Compute Decr regSP reg0 regB
+ , Load (ImmValue (5)) regC
+ , Push regC
+ , Compute Add regB reg0 regA
+ , Jump (Abs (1))
  , EndProg
  ]
 
-main = run [prog,prog,prog]
+main = run [prog]
